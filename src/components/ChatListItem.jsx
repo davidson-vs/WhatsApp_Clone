19feef33e17230a93b88c61eca008ba4cbe8bbee
davidson-vs <davidson.vieiras27@gmail.com>
2021-11-11 17:27:50 -0300

    Finalization of the left side of the aplication.

--- src/components/ChatListItem.jsx
+++ src/components/ChatListItem.jsx
@@ -1,19 +1,15 @@
 import React from "react"
 import "./ChatListItem.css"
-export default ({ onClick, active }) => {
+export default ({ onClick, active, data }) => {
    return (
       <div
          className={`chatlistitem ${active ? "active" : ""}`}
          onClick={onClick}
       >
-         <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJhvWpQrh3nIxmjLBQSyH5uu7OKpprR2b4-g&usqp=CAU"
-            alt="avatar"
-            className="chatlist-avatar"
-         />
+         <img src={data.image} alt="avatar" className="chatlist-avatar" />
          <div className="chatlistitem-lines">
             <div className="chatlistitem-line">
-               <div className="chatlistitem-name">Davidson Vieira</div>
+               <div className="chatlistitem-name">{data.title}</div>
                <div className="chatlistitem-date">19:00</div>
             </div>
             <div className="chatlistitem-line">
